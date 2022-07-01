1fb8f5488b70f4264c37b6284ecff784065cfe20
Mathenson <41267153+Mathenson@users.noreply.github.com>
2022-07-01 03:30:47 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  * Return: Always 1 (success)
  * 
  */
- int main(void)
- {
+int main(void)
+{
  	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, 59);
