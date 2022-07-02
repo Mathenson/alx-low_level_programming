7ac5d4015a6502e9700e9cc14a673c5d1e54954a
mathenson <femorialmartin02@gmail.com>
2022-07-02 14:45:14 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Code to print the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for(l = 'a' ; l <= 'z' ; l++) 
+	{
+		putchar(l);
+	}
+
+	return(0);
+}
