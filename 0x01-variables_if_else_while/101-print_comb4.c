ee79e7a0c9f64841898825dcd335e6226dbfc6fb
mathenson <femorialmartin02@gmail.com>
2022-07-03 11:44:44 -0700

    three combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Combining three numbers
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	int f, e, m;
+	
+	for (f = '0'; f <= '9'; f++)
+	{
+		for (e = '0'; e <= '9'; e++)
+		{
+			for (f = '0'; f <= '9'; f++)
+			{
+				if (f > e && e < m)
+				{
+					putchar(f);
+					putchar(e);
+					putchar(m);
+					if (f != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
