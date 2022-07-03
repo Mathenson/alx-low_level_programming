d3ceb3392daa65666f8f1f90c118d033f72643f4
mathenson <femorialmartin02@gmail.com>
2022-07-03 07:49:23 -0700

    excepting g and q in alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Excepting e and q in the alphabets output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		if(p != 'e' && p != 'q');
+			putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
