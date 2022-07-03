e2e127146dd302722e94cfffee4a55ca01216809
mathenson <femorialmartin02@gmail.com>
2022-07-03 10:17:41 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - combination
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
