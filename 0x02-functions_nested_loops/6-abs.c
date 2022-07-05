b78dea63697b01482070352101e149868ded0adb
mathenson <femorialmartin02@gmail.com>
2022-07-05 12:02:23 -0700

    printing absolute of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * _abs - Print tje absolute value of an integer
+ *
+ * @i: input number as integer
+ *
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (1 * -1);
+	}
+
+}
