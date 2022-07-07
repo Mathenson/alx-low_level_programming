844a12a62f87cfb586e444c343ee7c3b8002d931
mathenson <femorialmartin02@gmail.com>
2022-07-07 03:39:16 -0700

    check if inout is from 0 - 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if input is digit or not
+ * @c: integer to return
+ * Return: print 1 if c is of range (0 - 9) otherwise print 0
+ */
+int _isdigit(int c)
+{
+	int c;
+
+	for (c >= 48 && c <=  57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
