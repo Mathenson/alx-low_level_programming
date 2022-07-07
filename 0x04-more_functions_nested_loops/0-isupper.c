ac849a08e462a48d826531fe200b1f985c198fc0
mathenson <femorialmartin02@gmail.com>
2022-07-07 02:44:10 -0700

    check for Capitalization

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - check if the argument is lower ir upper case
+ * @c: input character
+ * Return: 1 if it is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This Section is dedicated to dive more dipper in nested loop
+These are learnt in the ALX FULL STACK SOFTWARE ENGINEERING COURSE:
