ccd27534931f6a7f14f73329ca2f83160794226a
mathenson <femorialmartin02@gmail.com>
2022-07-12 15:44:58 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - grenerates keygen
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+
+	}
+	printf("%\n", (2772 -c));
+	return (0);
+}
