d67a1b9902e3a90dd81fbc6b422c0d5a2504929f
mathenson <femorialmartin02@gmail.com>
2022-07-14 08:13:06 -0700

    Concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: destination file 
+ * @src: source.
+ * Return: the pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lo = vo = 0;
+
+	while (*(dest + lo) != '\0')
+	{
+		lo++;
+	}
+
+	while (vo >= 0)
+	{
+		*(dest + lo) = *(src + vo);
+		if (*(src + vo) == '\0')
+			break;
+		lo++;
+		vo++;
+
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This section is for more pointers arrays strings
+
