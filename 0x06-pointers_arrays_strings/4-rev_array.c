7b1649bcba7a88719f0fc5b6821a4037dfd76dcf
Mathenson <41267153+Mathenson@users.noreply.github.com>
2022-07-15 10:09:44 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,15 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tm;
+	int i, j, tmp;
 
 	for (i = 0; i < n - 1; i++)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
-			tm = *(a + j);
+			tmp = *(a + j);
 			*(a + j) = *(a + (j - 1));
-			*(a + (j - 1)) = tm;
+			*(a + (j - 1)) = tmp;
 		}
 	}
 }
