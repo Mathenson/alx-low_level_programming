e67f0723d8290a265845721c8eee2b1921f61400
mathenson <femorialmartin02@gmail.com>
2022-07-15 02:58:00 -0700

    change lower cse to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string
+ * Return: the pointer to dest
+ */
+
+char *string_toupper(char *s)
+{
+	int lt = 0;
+
+	while (*(s + lt) != '\0')
+	{
+		if ((*(s + lt) >= 97) && (*(s + lt) <= 122))
+			*(s + lt) = *(s + lt) - 32;
+		lt;
+	}
+	return (s);
+}
