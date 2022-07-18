3e00b21b6dd2dee1a3bbb952325488a127fa47a2
Mathenson <41267153+Mathenson@users.noreply.github.com>
2022-07-18 19:59:55 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 /**
  * _strchr - locates a character in a string
  * @s: string
- * @c: character 
+ * @c: character
  * Return: the pointer to the first occurence of the character c
  */
 char *_strchr(char *s, char c)
